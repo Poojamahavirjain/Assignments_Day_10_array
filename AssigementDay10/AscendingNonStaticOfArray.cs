ff1165f9f708dc2acd0742f4f5fd802dcfa0188b
pooja jain <poojamjain299@gmail.com>
2023-04-16 17:16:58 +0530

    Assigement Day 10

---
+++ AssigementDay10/AscendingNonStaticOfArray.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssigementDay10
+{
+    internal class AscendingNonStaticOfArray
+    {
+
+        public static void Main(string[] args)
+        {
+            int size;
+            Console.WriteLine("Enter the size of an array: ");
+            size = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the elements into an array: ");
+            int[] arr = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            AscendingNonStaticOfArray a = new AscendingNonStaticOfArray();
+            a.ascending(arr);
+
+        }
+        public void ascending(int[] arr)
+        {
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+
+                    if (arr[i] > arr[j])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+
+                    }
+                }
+                Console.Write(" " + arr[i]);
+            }
+
+        }
+
+    }
+}
---
+++ AssigementDay10/ReverseArrayNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssigementDay10
+{
+    internal class ReverseArrayNumber
+    {
+        static void Main(string[] args)
+        {
+
+            int[] num = { 20, 30, 40, 80, 90, 120 };
+
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+
+                Console.WriteLine(num[i]);
+            }
+        }
+
+    }
+}
